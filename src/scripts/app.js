8107fbf3bf55a655a36c966f3ffd512a266a3227
Antoine Weverbergh <weverant@me.com>
2021-10-10 01:14:52 +0200

    project setup

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,7 @@
-import hello from './components/hello';
-import {
-    name
-} from './components/hello2';
-import {} from './utils/helpers';
-import './utils/scrollbarWidth';
+import hello from "./components/hello";
+import { name } from "./components/hello2";
+import {} from "./utils/helpers";
+import "./utils/scrollbarWidth";
 
-console.info(`Hello ${hello.name}`);
-console.info(`Hello ${name()}`);
+// console.info(`Hello ${hello.name}`);
+// console.info(`Hello ${name()}`);
